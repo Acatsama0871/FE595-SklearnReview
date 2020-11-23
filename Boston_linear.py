4e8bbcce44db24ec023246145f6babdb9311d052
Haohang Li <hhl0871@outlook.com>
2020-11-23 01:42:36 -0500

    update first

--- Boston_linear.py
+++ Boston_linear.py
@@ -8,6 +8,7 @@ from sklearn.datasets import load_boston
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
 
+
 def main():
     # load dataset
     data = load_boston()
@@ -20,28 +21,33 @@ def main():
     model_linear.fit(dataX, dataY)
 
     # output fit result
-    pred =model_linear.predict(dataX)
-    print('-'*80)
-    print('Fit results:')
-    print('Coefficients:', model_linear.coef_, '\n')
-    print('The training MSE:', mean_squared_error(dataY, pred))
-    print('The coefficients of determination:', r2_score(dataY, pred))
-    print('-'*80, '\n\n')
+    pred = model_linear.predict(dataX)
+    print("-" * 80)
+    print("Fit results:", "\n")
+    print("Coefficients:")
+    print(tabulate(zip(feature_names, model_linear.coef_), tablefmt="simple"), "\n\n")
+    print("The training MSE:", mean_squared_error(dataY, pred))
+    print("The coefficients of determination:", r2_score(dataY, pred))
+    print("-" * 80, "\n\n")
 
     # importance of predictors
     coef_abs = np.abs(model_linear.coef_)
     temp = sorted(coef_abs, reverse=True)
     ordered_index = [np.where(coef_abs == i)[0][0] for i in temp]
-    ordered_features = [feature_names[i] for i in ordered_index]
-    ordered_coef = [round(model_linear.coef_[i], 4) for i in ordered_index]
+    ordered_features = feature_names[ordered_index]
+    ordered_coef = np.round(model_linear.coef_[ordered_index], 4)
     ordered_coef_abs = np.abs(ordered_coef)
 
     # output
-    print('Feature Importance: from greatest to least','\n')
-    print(tabulate(np.array([ordered_features, ordered_coef_abs, ordered_coef]).T,
-                   headers=['Feature Name', 'Abs(Coefficients)', 'Coefficients']))
+    print("Feature Importance: from greatest to least", "\n")
+    print(
+        tabulate(
+            np.array([ordered_features, ordered_coef_abs, ordered_coef]).T,
+            headers=["Feature Name", "Abs(Coefficients)", "Coefficients"],
+            tablefmt="github",
+        )
+    )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
